1dffca6f1ab4548fd2264cb52bdda5b4afe343a0
c_bvaalb <btvaalburg@gmail.com>
2018-10-26 10:10:37 -0600

    +admin show ordering

--- setlists/admin.py
+++ setlists/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from setlists.models import *
 
+
+
+class ShowAdmin(admin.ModelAdmin):
+
+    ordering = ('-date',)
+
+# Register your models here.
+
+
 admin.site.register(Song)
 admin.site.register(ShowSong)
-admin.site.register(Show)
+admin.site.register(Show, ShowAdmin)
 admin.site.register(Artist)
 admin.site.register(Tour)
 admin.site.register(Venue)
 admin.site.register(Album)
 admin.site.register(UserProfile)
-
-# Register your models here.
