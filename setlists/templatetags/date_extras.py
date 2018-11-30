c066ae5cfc64db0ebc90d2e87389e8b5720c915e
Brent Vaalburg <btvaalburg@gmail.com>
2018-11-29 23:25:44 -0700

    +date abbrev

--- setlists/templatetags/date_extras.py
+++ setlists/templatetags/date_extras.py
@@ -6,8 +6,8 @@ register = template.Library()
 
 @register.filter
 def month_name(month_number):
-    return calendar.month_name[month_number]
+    return calendar.month_abbr[month_number]
 
 @register.filter
 def weekday_name(weekday_number):
-    return calendar.day_name[weekday_number-1]
+    return calendar.day_abbr[weekday_number-1]
