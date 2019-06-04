1437ca4c13b08b34ce3dc5f43f2754c68df494cf
Brent Vaalburg <btvaalburg@gmail.com>
2019-06-03 22:47:27 -0600

    social and oauth keys moved to base.py settings

--- sts9_3/settings/prod.py
+++ sts9_3/settings/prod.py
@@ -14,15 +14,6 @@ ALLOWED_HOSTS = [
     '.squaresandcubes.net'
 ]
 
-# google social auth keys
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
-# facebook social auth keys
-SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
-SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')
-#twitter social auth keys
-SOCIAL_AUTH_TWITTER_KEY = os.environ.get('SOCIAL_AUTH_TWITTER_KEY')
-SOCIAL_AUTH_TWITTER_SECRET = os.environ.get('SOCIAL_AUTH_TWITTER_SECRET')
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
