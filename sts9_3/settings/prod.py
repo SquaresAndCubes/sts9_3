44d377570651b50b3e09e57477067f0bd90b7361
Brent Vaalburg <btvaalburg@gmail.com>
2019-06-04 21:33:24 -0600

    prod static test

--- sts9_3/settings/prod.py
+++ sts9_3/settings/prod.py
@@ -35,4 +35,6 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
